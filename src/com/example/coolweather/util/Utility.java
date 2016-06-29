d76e9dbec6d13b8fd923cd63d1801df123b0465e
Xiao <chenxiaolin@hust.edu.cn>
2016-06-30 02:36:13 +0800

    加入显示天气信息的功能。

--- src/com/example/coolweather/util/Utility.java
+++ src/com/example/coolweather/util/Utility.java
@@ -1,9 +1,18 @@
 package com.example.coolweather.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONObject;
+
 import com.example.coolweather.model.City;
 import com.example.coolweather.model.County;
 import com.example.coolweather.model.Province;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -29,17 +38,18 @@ public class Utility {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 解析和处理服务器返回的市级数据
 	 */
-	public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB, String response, int provinceId){
-		if(!TextUtils.isEmpty(response)){
+	public static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int provinceId) {
+		if (!TextUtils.isEmpty(response)) {
 			String[] allCities = response.split(",");
 
 			Log.d("xiao", allCities.toString());
-			if(allCities != null && allCities.length > 0){
-				for(String c : allCities){
+			if (allCities != null && allCities.length > 0) {
+				for (String c : allCities) {
 					String[] array = c.split("\\|");
 					City city = new City();
 					city.setCityCode(array[0]);
@@ -48,19 +58,20 @@ public class Utility {
 					coolWeatherDB.saveCity(city);
 				}
 				return true;
-			}			
+			}
 		}
-		return false;		
-	}	
-	
+		return false;
+	}
+
 	/**
 	 * 解析和处理服务器返回的县级数据
 	 */
-	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB, String response, int cityId){
-		if(!TextUtils.isEmpty(response)){
+	public static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,
+			String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
 			String[] allCounties = response.split(",");
-			if(allCounties != null && allCounties.length > 0){
-				for(String c : allCounties){
+			if (allCounties != null && allCounties.length > 0) {
+				for (String c : allCounties) {
 					String[] array = c.split("\\|");
 					County county = new County();
 					county.setCountyCode(array[0]);
@@ -72,5 +83,46 @@ public class Utility {
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * 解析服务器返回的JSON数据，并将解析后的数据存储到本地
+	 */
+	public static void handleWeatherResponse(Context context, String response) {
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weather = weatherInfo.getString("weather");
+			String ptime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+					weather, ptime);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 将服务器返回的天气信息存储到SharedPreferences文件中
+	 */
+	public static void saveWeatherInfo(Context context, String cityName,
+			String weatherCode, String temp1, String temp2, String weather,
+			String ptime) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager
+				.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather", weather);
+		editor.putString("ptime", ptime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
 	}
 }
